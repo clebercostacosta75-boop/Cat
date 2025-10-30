9a2c676cf48c4d3cd01f46af992439c2145bbf56
Cleber Costa <clebercostacosta75@gmail.com>
2025-10-30 00:48:08 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,7 +1,6 @@
 import Dashboard from './pages/Dashboard';
 import Import from './pages/Import';
 import Schedule from './pages/Schedule';
-import Instructors from './pages/Instructors';
 import Courses from './pages/Courses';
 import Reports from './pages/Reports';
 import Layout from './Layout.jsx';
@@ -11,7 +10,6 @@ export const PAGES = {
     "Dashboard": Dashboard,
     "Import": Import,
     "Schedule": Schedule,
-    "Instructors": Instructors,
     "Courses": Courses,
     "Reports": Reports,
 }
