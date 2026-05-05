9e32e17af0b61d585d52f925e9f0dc81d9a8a6db
Cleber Costa <clebercostacosta75@gmail.com>
2026-05-05 12:15:41 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -62,7 +62,6 @@ import Companies from './pages/Companies';
 import Contractors from './pages/Contractors';
 import CourseCategories from './pages/CourseCategories';
 import Courses from './pages/Courses';
-import Dashboard from './pages/Dashboard.jsx';
 import EmailTemplates from './pages/EmailTemplates';
 import Home from './pages/Home';
 import Import from './pages/Import';
@@ -96,7 +95,6 @@ export const PAGES = {
     "Contractors": Contractors,
     "CourseCategories": CourseCategories,
     "Courses": Courses,
-    "Dashboard": Dashboard,
     "EmailTemplates": EmailTemplates,
     "Home": Home,
     "Import": Import,
